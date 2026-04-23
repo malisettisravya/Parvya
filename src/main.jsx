0b3e13e7530ab29a3233dcfe71833d4bb3e9b942
malisettisravya <sravyamalisetti@gmail.com>
2026-04-24 03:34:08 +0530

    corrected import paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { CartProvider } from "./Context/Cartcontext"
+import { CartProvider } from "./context/Cartcontext"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
